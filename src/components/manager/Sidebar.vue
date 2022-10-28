459b8ec82ac6652cb4baebfda7d0d440a382310c
heyjorgesilva <github@jorgesilva.com.br>
2022-10-28 10:00:19 -0300

    tradução

--- src/components/manager/Sidebar.vue
+++ src/components/manager/Sidebar.vue
@@ -3,7 +3,7 @@
   :class="managerStore.sidebar ? ['translate-x-[0]']: ['translate-x-[-100%]']">
     <div
       class="bg-white dark:bg-dark-grey min-h-[calc(100vh-80px)] w-64 lg:w-[300px] pb-10 hidden sm:flex sm:flex-col border-r border-r-lines-light dark:border-r-lines-dark">
-      <p class="text-medium-grey text-xs font-bold py-4 px-6">ALL BOARDS ({{ boardsStore.boards.length }})</p>
+      <p class="text-medium-grey text-xs font-bold py-4 px-6">TODOS OS BOARDS ({{ boardsStore.boards.length }})</p>
       <div @click="onClickBoard(index)" v-for="(board, index) in boardsStore.boards" :key="index"
         class="flex items-center gap-4 w-11/12 rounded-r-full px-6 py-3 cursor-pointer text-medium-grey font-bold   "
         :class="board?.name === boardsStore?.getCurrentBoard?.name ?
@@ -16,7 +16,7 @@
       <div @click="createNewBoard()"
         class=" flex items-center gap-4 w-11/12 rounded-r-full px-6 py-3 cursor-pointer font-bold fill-main-purple text-main-purple hover:bg-medium-grey/10">
         <IconBoard />
-        <span>+ Create New Board</span>
+        <span>+ Novo board</span>
       </div>
       <div class="flex flex-col gap-2 mt-auto">
         <DarkModeSwitch class="w-10/12 mx-auto " />
